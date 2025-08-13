75b8bfe6b45ba67a0871e9aa26595d0254cf601b
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2025-08-13 23:59:17 +0500

    Restructure index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./css/style.css";
 
+import tenguIcon from "./img/goblin.png";
+
+function tenguImage() {
+  const tengu = document.createElement('img');
+  tengu.classList.add('tengu');
+  tengu.src = tenguIcon;
+  tengu.alt = "Тенгу";
+  return tengu
+}
+
+document.body.appendChild(tenguImage());
+
 import "./js/app";
